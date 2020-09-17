00bf8e575cb7e6ca3bbcc24e52081829fec08286
Robert Moss <rmoss92@gmail.com>
2020-09-17 02:07:04 -0700

    Use of broadcasting in particle filter

--- src/particle_filter.jl
+++ src/particle_filter.jl
@@ -2,9 +2,10 @@ using Distributions, LinearAlgebra
 
 struct POMDP γ; 𝒮; 𝒜; 𝒪; T; R; O end
 
-function particle_filter(belief::Vector, 𝒫::POMDP, a, o)
+function particle_filter(𝐛::Vector, 𝒫::POMDP, a, o)
     (T, O) = (𝒫.T, 𝒫.O)
-    𝐬′ = [rand(T(s, a)) for s in belief]
-    𝐰 = [O(a, s′, o) for s′ in 𝐬′]
-    return 𝐬′[rand(Categorical(normalize(𝐰,1)), length(𝐬′))]
+    𝐬′ = rand.(T.(𝐛, a))
+    𝐰 = O.(a, 𝐬′, o)
+    D = Categorical(normalize(𝐰, 1))
+    return 𝐬′[rand(D, length(𝐬′))]
 end
