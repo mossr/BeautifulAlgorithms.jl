e3b2567f3922beec6bf3fb27ebc8822eb87ef727
Robert Moss <rmoss92@gmail.com>
2020-09-19 10:48:13 -0700

    Extra space in multi-layer neural network

--- src/multi_layer_neural_network.jl
+++ src/multi_layer_neural_network.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 function multi_layer_neural_network(x, 𝐖, φ, 𝐠)
     𝐡ᵢ = φ(x)
     for (i,g) in enumerate(𝐠)
-        𝐡ᵢ =  map(𝐰ⱼ -> g(𝐰ⱼ ⋅ 𝐡ᵢ), 𝐖[i])
+        𝐡ᵢ = map(𝐰ⱼ -> g(𝐰ⱼ ⋅ 𝐡ᵢ), 𝐖[i])
     end
     𝐡ᵢ ⋅ last(𝐖)
 end
