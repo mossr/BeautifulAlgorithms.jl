559d8c8b6cc4425105f1cfa3b9752696dfe8233d
Robert Moss <rmoss92@gmail.com>
2021-10-28 21:55:29 -0700

    Fixed linear regression

--- src/linear_regression.jl
+++ src/linear_regression.jl
@@ -1,11 +1,6 @@
 using LinearAlgebra
 
-function linear_regression(X::Vector{<:Real}, y)
-    θ = pinv(X)*y
-    return x -> θ'x
-end
-
-function linear_regression(X::Vector{<:Vector}, y)
+function linear_regression(X, y)
     𝐗 = mapreduce(x->[1; x]', vcat, X)
     𝛉 = pinv(𝐗)*y
     return x -> 𝛉⋅[1; x]
