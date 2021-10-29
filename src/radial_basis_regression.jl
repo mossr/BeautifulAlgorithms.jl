b559fd40f5dba39884335e961f1cc14775078ccf
Robert Moss <rmoss92@gmail.com>
2021-10-29 00:23:35 -0700

    Simplified radial basis regression

--- src/radial_basis_regression.jl
+++ src/radial_basis_regression.jl
@@ -2,8 +2,8 @@ using LinearAlgebra
 
 function radial_basis_regression(X, y; σ=1, p=2)
     ψ = r->exp(-r^2 / (2σ^2))
-    bases = [x->ψ(norm(x - c, p)) for c in X]
-    B = [b(x) for x in X, b in bases]
-    θ = pinv(B)*y
-    return x -> sum(θ[i] * bases[i](x) for i in 1:length(θ))
+    φ(x) = [ψ(norm(x - c, p)) for c in X]
+    B = mapreduce(x->φ(x)', vcat, X)
+    θ = B\y
+    return x -> φ(x)'θ
 end
