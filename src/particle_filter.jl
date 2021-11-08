2c5210ef26288238c8ad23afd1484716695ce354
Robert Moss <rmoss92@gmail.com>
2021-11-07 17:16:17 -0800

    Changed particle filter variable name to reflect 'collection of states'

--- src/particle_filter.jl
+++ src/particle_filter.jl
@@ -2,9 +2,9 @@ using Distributions, LinearAlgebra
 
 struct POMDP γ; 𝒮; 𝒜; 𝒪; T; R; O end
 
-function particle_filter(𝐛::Vector, 𝒫::POMDP, a, o)
+function particle_filter(𝐬::Vector, 𝒫::POMDP, a, o)
     (T, O) = (𝒫.T, 𝒫.O)
-    𝐬′ = rand.(T.(𝐛, a))
+    𝐬′ = rand.(T.(𝐬, a))
     𝐰 = O.(a, 𝐬′, o)
     D = Categorical(normalize(𝐰, 1))
     return 𝐬′[rand(D, length(𝐬′))]
